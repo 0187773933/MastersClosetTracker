5d6c388c1e7f9cdb0bbb388baefb934791ff692b
0187773933 <collincerbus@student.olympic.edu>
2023-07-03 18:41:51 -0400

    3

--- dockerRun.sh
+++ dockerRun.sh
@@ -3,7 +3,7 @@ APP_NAME="public-mct-db-server"
 sudo docker rm -f $APP_NAME || echo ""
 id=$(sudo docker run -dit \
 --name $APP_NAME \
---mount type=bind,source=/home/morphs/mct/config.json,target=/home/morphs/mct/config.json \
+--mount type=bind,source=/home/morphs/mct/save_files/config.json,target=/home/morphs/mct/config.json \
 -v /home/morphs/mct/save_files/:/home/morphs/mct/save_files \
 -p 5950:5950 \
 $APP_NAME config.json)
---
+++ gitPull.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+# git clone -b decentralized https://github.com/0187773933/MastersClosetTracker.git MastersClosetTrackerDecentralized
