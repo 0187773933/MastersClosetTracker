31181ac48d73b1b22a6baf1b5c25429184eb58d6
0187773933 <collincerbus@student.olympic.edu>
2023-07-04 08:58:35 -0400

    5

--- gitPull.sh
+++ gitPull.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 # git clone -b decentralized https://github.com/0187773933/MastersClosetTracker.git MastersClosetTrackerDecentralized
+git fetch origin
+git reset --hard origin/decentralized
