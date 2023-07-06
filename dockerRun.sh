c100684692cae4f153803f8e89ded5469d2076b6
0187773933 <collincerbus@student.olympic.edu>
2023-07-06 08:38:25 -0400

    6

--- dockerRun.sh
+++ dockerRun.sh
@@ -5,6 +5,7 @@ id=$(sudo docker run -dit \
 --name $APP_NAME \
 --mount type=bind,source=/home/morphs/mct/config.json,target=/home/morphs/mct/config.json \
 -v /home/morphs/mct/save_files/:/home/morphs/mct/save_files \
+-v /home/morphs/mct/logs/:/home/morphs/mct/logs \
 -p 5950:5950 \
 $APP_NAME config.json)
 sudo docker logs -f $id
