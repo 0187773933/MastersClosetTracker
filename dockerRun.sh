8076c1eec3beac4dedc97bf338821522ebf7fcdc
0187773933 <collincerbus@student.olympic.edu>
2023-07-30 17:33:20 -0400

    38

--- dockerRun.sh
+++ dockerRun.sh
@@ -3,6 +3,7 @@ APP_NAME="public-mct-db-server"
 sudo docker rm -f $APP_NAME || echo ""
 id=$(sudo docker run -dit \
 --name $APP_NAME \
+--restart='always' \
 --mount type=bind,source=/home/morphs/mct/config.json,target=/home/morphs/mct/config.json \
 -v /home/morphs/mct/save_files/:/home/morphs/mct/save_files \
 -v /home/morphs/mct/logs/:/home/morphs/mct/logs \
